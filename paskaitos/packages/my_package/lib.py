7e18dc3f417af20053bfa6a2977185251f7baca4
tomasrasymas <tomas@dutrys.com>
2018-01-16 16:12:56 +0200

    Changes

---
+++ paskaitos/packages/my_package/lib.py
@@ -0,0 +1,18 @@
+def add(a, b):
+   """This program adds two
+   numbers and return the result"""
+
+   result = a + b
+   return result
+
+
+def subtract(a, b):
+   """This program subtracts two
+   numbers and return the result"""
+
+   result = a - b
+   return result
+
+class Person:
+	def __init__(self, name):
+		self.name = name
