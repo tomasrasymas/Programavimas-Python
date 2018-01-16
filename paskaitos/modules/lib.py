b4e60283d8f49b8c610130dd3fbd31bf1027bd3f
tomasrasymas <tomas@dutrys.com>
2018-01-16 15:58:44 +0200

    Changes

---
+++ paskaitos/modules/lib.py
@@ -0,0 +1,14 @@
+def add(a, b):
+   """This program adds two
+   numbers and return the result"""
+
+   result = a + b
+   return result
+
+
+def subtract(a, b):
+   """This program subtracts two
+   numbers and return the result"""
+
+   result = a - b
+   return result
---
+++ paskaitos/modules/lib_with_name.py
@@ -0,0 +1,20 @@
+def add(a, b):
+   """This program adds two
+   numbers and return the result"""
+
+   result = a + b
+   return result
+
+
+def subtract(a, b):
+   """This program subtracts two
+   numbers and return the result"""
+
+   result = a - b
+   return result
+
+
+if __name__ == "__main__":
+    print("Modulis yra vykdomas")
+else:
+    print("Modulis yra importuojamas")
